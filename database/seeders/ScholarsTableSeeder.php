1677a90aaaf9804aa3f4e5e97ee7e00e4099b81a
stsimskrad <102267093+stsimskrad@users.noreply.github.com>
2022-09-22 04:41:53 +0800

    we

--- database/seeders/ScholarsTableSeeder.php
+++ database/seeders/ScholarsTableSeeder.php
@@ -28,7 +28,7 @@ class ScholarsTableSeeder extends Seeder
                 'lrn' => '000001',
                 'program_id' => 1,
                 'spas_id' => 'SPAS-000001',
-                'status_id' => 38,
+                'status_id' => 31,
                 'updated_at' => '2022-09-21 07:14:08',
             ),
             1 => 
@@ -40,7 +40,7 @@ class ScholarsTableSeeder extends Seeder
                 'lrn' => '000002',
                 'program_id' => 1,
                 'spas_id' => 'SPAS-000002',
-                'status_id' => 38,
+                'status_id' => 31,
                 'updated_at' => '2022-09-21 07:36:03',
             ),
         ));

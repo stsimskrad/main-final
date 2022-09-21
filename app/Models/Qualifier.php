46d1ec701f2d67fe7052448d3f2851eba27e6c32
stsimskrad <102267093+stsimskrad@users.noreply.github.com>
2022-09-22 07:50:39 +0800

    Update Qualifier.php

--- app/Models/Qualifier.php
+++ app/Models/Qualifier.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Qualifier extends Model
 {
     use HasFactory;
+
+    public function region()
+    {
+        return $this->belongsTo('App\Models\LocationRegion', 'region_code', 'code');
+    }
+
+    public function province()
+    {
+        return $this->belongsTo('App\Models\LocationProvince', 'province_code', 'code');
+    }
+
+    public function municipality()
+    {
+        return $this->belongsTo('App\Models\LocationMunicipality', 'municipality_code', 'code');
+    }
+
+    public function barangay()
+    {
+        return $this->belongsTo('App\Models\LocationBarangay', 'barangay_code', 'code');
+    }
+
+    public function program()
+    {
+        return $this->belongsTo('App\Models\ListProgram', 'program_id', 'id');
+    }  
 }
